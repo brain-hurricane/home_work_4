a8ba8cfaef759530f957a5587403a8b1e0ac91a8
Oleg Danilov <brain.hurricane@hotmail.com>
2023-07-24 14:49:02 +0300

    edit str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 
 def string_to_upper(string):
     return string.upper()
+
+
+def string_to_title(string):
+    """
+    делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    """
+
+    string_list = string.split(" ")
+    string_list_title = [s.title() for s in string_list]
+    result = " ".join(string_list_title)
+
+    return result
