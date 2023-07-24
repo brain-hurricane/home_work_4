986d225eee8503f9c284cd2bc7a12111798a9651
Олег <98827114+brain-hurricane@users.noreply.github.com>
2023-07-24 14:59:12 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,18 +1,8 @@
 
 def string_to_upper(string):
     """
-    string to upper case
+    string to upper case 111
     """
     return string.upper()
 
 
-def string_to_title(string):
-    """
-    делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
-    """
-
-    string_list = string.split(" ")
-    string_list_title = [s.title() for s in string_list]
-    result = " ".join(string_list_title)
-
-    return result
